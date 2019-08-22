c43c07d661aab83ad8ac65f45826afb903cbf585
LỤC THỚI SANG <devbrownbear@gmail.com>
2019-08-22 09:58:51 +0700

    Fix loi fetch data partList

--- src/app/admin-layout/question/question.component.ts
+++ src/app/admin-layout/question/question.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Answer } from 'src/app/models/answer';
-import { QuestiontypeService } from 'src/app/services/questiontype.service';
-import { Form, NgForm } from '@angular/forms';
-import { Question } from 'src/app/models/question';
-import { QuestionType } from 'src/app/models/question_type';
-import { SubjectService } from 'src/app/services/subject.service';
-import { Subject } from 'src/app/models/subject';
-import { QuestionService } from 'src/app/services/question.service';
-import { Part } from 'src/app/models/part';
+import {Component, OnInit} from '@angular/core';
+import {Answer} from 'src/app/models/answer';
+import {QuestiontypeService} from 'src/app/services/questiontype.service';
+import {Question} from 'src/app/models/question';
+import {QuestionType} from 'src/app/models/question_type';
+import {SubjectService} from 'src/app/services/subject.service';
+import {Subject} from 'src/app/models/subject';
+import {QuestionService} from 'src/app/services/question.service';
+import {Part} from 'src/app/models/part';
 
 
 @Component({
@@ -17,58 +16,61 @@ import { Part } from 'src/app/models/part';
 })
 export class QuestionComponent implements OnInit {
 
-  loading=false;
+  loading = false;
   answer1: Answer;
   answer2: Answer;
   questionTypes: QuestionType[];
-  question: any={};
+  question: any = {};
   questions: Question[];
   typeQuestion: QuestionType;
   questionTypeId: number;
   subjects: Subject[];
-  selectedSubject: Subject;
+  selectedSubject: any = {};
   parts: Part[];
-  public answers: any[]=[];
-  constructor(private questionTypeService : QuestiontypeService, private subjectSevice: SubjectService, private questionService: QuestionService) {
+  public answers: any[] = [];
+
+  constructor(private questionTypeService: QuestiontypeService, private subjectSevice: SubjectService, private questionService: QuestionService) {
     this.answers.push(this.answer1, this.answer2);
 
-   }
+  }
 
   ngOnInit() {
     this.loadQuestions();
-    this.questionTypeService.getTypes().subscribe(data=> {
-     
-     this.questionTypes=data;
-     
-   });
-   this.loadSubjects();
-   
+    this.questionTypeService.getTypes().subscribe(data => {
+
+      this.questionTypes = data;
+
+    });
+    this.loadSubjects();
+
   }
 
-  addQuestion(){
-    
+  addQuestion() {
+
     //gán typeQuestion với questionTypeId vừa tìm được
     console.log(this.question.questionType);
   }
 
-  loadSubjects(){
-    this.subjectSevice.getSubject().subscribe(data=>this.subjects=data);
+  loadSubjects() {
+    this.subjectSevice.getSubject().subscribe(data => this.subjects = data);
   }
-  getPart(){
-    
+
+  getPart() {
+
     //get đối tượng subject là selectedSubject
-    this.parts=this.selectedSubject.partList;
-   
+    this.parts = this.selectedSubject.partList;
+
 
   }
-  loadQuestions(){
-    this.loading=true;
-    this.questionService.getQuestions().subscribe(data=>{
-      
-      this.questions=data;
-      this.loading=false;
+
+  loadQuestions() {
+    this.loading = true;
+    this.questionService.getQuestions().subscribe(data => {
+
+      this.questions = data;
+      this.loading = false;
     });
-    
+
   }
 
 
--- src/app/models/subject.ts
+++ src/app/models/subject.ts
@@ -1,5 +1,7 @@
-import { Part } from './part';
+import {Part} from './part';
 
-export class Subject{
-    constructor(id: number, subjectName: string, description: string, imgUrl: string, parentId: number, priority: number, partList: Part[], deleted: boolean){}
+export class Subject {
+  constructor(id: number, subjectName: string, description: string, imgUrl: string, parentId: number, priority: number, partList: Part[], deleted: boolean) {
+  }
+
 }
