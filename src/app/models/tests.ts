744bb6d0872bd39ce4a2747df1acc7531eaf8a88
LỤC THỚI SANG <devbrownbear@gmail.com>
2019-08-29 09:21:24 +0700

    Xay dung giao dien quan ly test

--- src/app/models/tests.ts
+++ src/app/models/tests.ts
@@ -1,13 +1,15 @@
-import { Question } from 'src/app/models/question';
-export class Tests{
-    id: number;
-    testTitle:string;
-    testTime: number
-    deleted: boolean;
-    questionList: Question[];
-    constructor(testTitle: string, testTime: number, deleted: boolean, questionList: Question[]){
-        this.testTitle=testTitle;
-        this.testTime=testTime;
-        this.deleted=deleted;
-    }
+import {Question} from 'src/app/models/question';
+
+export class Tests {
+  id: number;
+  testTitle: string;
+  testTime: number;
+  deleted: boolean;
+  questionList: Question[];
+
+  constructor(testTitle: string, testTime: number, deleted: boolean, questionList: Question[]) {
+    this.testTitle = testTitle;
+    this.testTime = testTime;
+    this.deleted = deleted;
+  }
 }
